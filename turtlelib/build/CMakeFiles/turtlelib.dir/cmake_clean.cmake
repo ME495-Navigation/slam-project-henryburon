e57a1d644640f5f3c5e22f8a7fa9b259cdac83ef
Henry Buron <henryburon34@gmail.com>
2024-01-12 15:40:02 -0600

    Started with CMake on Task B

---
+++ turtlelib/build/CMakeFiles/turtlelib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtlelib.dir/src/geometry.cpp.o"
+  "CMakeFiles/turtlelib.dir/src/geometry.cpp.o.d"
+  "libturtlelib.a"
+  "libturtlelib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtlelib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
