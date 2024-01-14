f8a3a6cdf32df5585402193e2916819e41f5e0f4
Henry Buron <henryburon34@gmail.com>
2024-01-14 16:32:33 -0600

    Added the function and test for reading the TWist2D

--- turtlelib/src/se2d.cpp
+++ turtlelib/src/se2d.cpp
@@ -1,16 +1,34 @@
 #include "turtlelib/se2d.hpp"
 #include <cmath>
 #include <iostream>
+#include <sstream>
 
 
 namespace turtlelib {
 
-    std::ostream &operator<<(std::ostream &os, const Twist2D &tw)
+    std::ostream & operator<<(std::ostream & os, const Twist2D & tw)
     {
         // Print the Twist2D in the format [w x y]
         return os << "[" << tw.omega << " " << tw.x << " " << tw.y << "]";
     }
 
+    std::istream & operator>>(std::istream & is, Twist2D & tw)
+    {
+        // Read the Twist2D in the format [w x y] or w x y
+        char c1 = is.peek();
 
+        if (c1 == '[')
+        {
+            is.get();
+            is >> tw.omega >> tw.x >> tw.y;
+            is.get();
+        }
+        else
+        {
+            is >> tw.omega >> tw.x >> tw.y;
+        }
+        return is;
+
+    }
 
 }
