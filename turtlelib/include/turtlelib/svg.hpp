09033ae997796db6eab965cf2c83eab8b3f6b12d
Henry Buron <henryburon34@gmail.com>
2024-01-20 12:52:26 -0600

    started svg file

---
+++ turtlelib/include/turtlelib/svg.hpp
@@ -0,0 +1,47 @@
+#ifndef TURTLELIB_SVG_INCLUDE_GUARD_HPP
+#define TURTLELIB_SVG_INCLUDE_GUARD_HPP
+/// \file
+/// \brief Visualization in svg
+
+#include<iosfwd>
+#include"turtlelib/geometry2d.hpp"
+#include"turtlelib/se2d.hpp"
+
+namespace turtlelib
+{
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+#endif
