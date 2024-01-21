af26936cfefcecdd1d01b7682251735885f41c3d
Henry Buron <henryburon34@gmail.com>
2024-01-21 03:38:52 -0600

    DrawPoint, DrawVector, and DrawCoordinate frame all work correctly. Next step is to make test and transforms

--- turtlelib/src/svg.cpp
+++ turtlelib/src/svg.cpp
@@ -9,22 +9,29 @@ namespace turtlelib
 {
     void Svg::DrawPoint(double cx, double cy, const std::string &pcolor, std::ofstream &outFile)
     {
-        // Make a 2D translation object
-        turtlelib::Transform2D turtlelibCoordinates(turtlelib::Vector2D{408, 528});
-
-        // Turn point into Vector2D object
-        turtlelib::Vector2D originalPoint{cx, cy};
-
-        // Apply the transformation
+        // Offset the point
+        turtlelib::Transform2D turtlelibCoordinates(turtlelib::Vector2D{4.25, 5.5});
+        turtlelib::Vector2D originalPoint{cx, -cy};
         turtlelib::Vector2D transformedPoint = turtlelibCoordinates(originalPoint);
 
-        outFile << "<circle cx=\"" << transformedPoint.x << "\" cy=\"" << transformedPoint.y
+        // Convert from inches to pixels
+        double x = transformedPoint.x * 96;
+        double y = transformedPoint.y * 96;
+        
+        outFile << "<circle cx=\"" << x << "\" cy=\"" << y
                 << "\" r=\"3\" stroke=\"" << pcolor << "\" fill=\"" << pcolor << "\" stroke-width=\"1\" />\n";
     }
 
     void Svg::DrawVector(double x1, double x2, double y1, double y2, const std::string &vcolor, std::ofstream &outFile)
     {
+        // Convert from inches to pixels
+        x1 = x1 * 96;
+        x2 = x2 * 96;
+        y1 = -y1 * 96;
+        y2 = -y2 * 96;
+        
         // Make a 2D translation object
+        // The -PI/2 rotation fixes the left-handedness of the viewBox's coordinate system
         turtlelib::Transform2D turtlelibCoordinates(turtlelib::Vector2D{408, 528});
 
         // Turn both head and tail into Vector2D objects
@@ -51,6 +58,10 @@ namespace turtlelib
         // Turtlelib coordinates are obtained through DrawVector
         Svg::DrawVector(head_xc_x, tail_x, head_yc_x, tail_y, "red", outFile);
         Svg::DrawVector(head_xc_y, tail_x, head_yc_y, tail_y, "green", outFile);
+
+        // Convert from inches to pixels
+        tail_x = tail_x * 96;
+        tail_y = -tail_y * 96;
 
         // Convert tail coordinates turtlelib coordinates
         turtlelib::Transform2D turtlelibCoordinates(turtlelib::Vector2D{408,528});
