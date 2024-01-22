b7716927785ad1e3b7ff91bedd6625a63a888fa5
Henry Buron <henryburon34@gmail.com>
2024-01-22 16:19:29 -0600

    Teleport service works. Need to remember to source terminal where I'm calling it

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -10,6 +10,7 @@
 #include "tf2_ros/transform_broadcaster.h"
 #include "tf2/LinearMath/Quaternion.h"
 #include "geometry_msgs/msg/transform_stamped.hpp"
+#include "nusim/srv/teleport.hpp"
 
 using namespace std::chrono_literals;
 
@@ -18,9 +19,17 @@ class Nusim : public rclcpp::Node
 public:
   Nusim() : Node("nusim"), timestep_(0)
   {
-    // Declare rate parameter with a default value of 200. There's another way to do this. Check documentation if needed.
+    // Declare parameters. There may be another way to do this...
     declare_parameter("rate", 200);
+    declare_parameter("x0", 0.0);
+    declare_parameter("y0", 0.0);
+    declare_parameter("theta0", 0.0);
+
+
     rate_ = get_parameter("rate").get_parameter_value().get<int>();
+    x0_ = get_parameter("x0").get_parameter_value().get<double>();
+    y0_ = get_parameter("y0").get_parameter_value().get<double>();
+    theta0_ = get_parameter("theta0").get_parameter_value().get<double>();
 
     // Create a publisher for UInt64 messages on the "~/timestep" topic.
     timestep_publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
@@ -32,9 +41,17 @@ public:
     reset_service = this->create_service<std_srvs::srv::Empty>(
         "~/reset", std::bind(&Nusim::reset_callback, this, std::placeholders::_1, std::placeholders::_2));
 
+    // Create teleport service
+    teleport_service = this->create_service<nusim::srv::Teleport>(
+      "~/teleport", std::bind(&Nusim::teleport_callback, this, std::placeholders::_1, std::placeholders::_2));
+
     // Initialize the the transform broadcaster
     tf_broadcaster_ =
       std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+    x_ = x0_;
+    y_ = y0_;
+    theta_ = theta0_;
   }
 
 private:
@@ -59,13 +76,13 @@ private:
     t.child_frame_id = "red/base_footprint";
 
     // Turtle only exists in 2D, so we set z coordinate to 0
-    t.transform.translation.x = 0.0;
-    t.transform.translation.y = 0.0;
+    t.transform.translation.x = x_;
+    t.transform.translation.y = y_;
     t.transform.translation.z = 0.0;
 
     // Likewise, turtle can only rotate around one axis -- z
     tf2::Quaternion q;
-    q.setRPY(0,0,0.0);
+    q.setRPY(0,0,theta_);
     t.transform.rotation.x = q.x();
     t.transform.rotation.y = q.y();
     t.transform.rotation.z = q.z();
@@ -76,11 +93,25 @@ private:
 
   }
 
-  void reset_callback(const std::shared_ptr<std_srvs::srv::Empty::Request>,
-                      std::shared_ptr<std_srvs::srv::Empty::Response>)
+  void reset_callback(
+    const std::shared_ptr<std_srvs::srv::Empty::Request>,
+    std::shared_ptr<std_srvs::srv::Empty::Response>)
   {
     timestep_ = 0;
+    x_ = x0_;
+    y_ = y0_;
+    theta_ = theta0_;
   }
+
+  void teleport_callback(
+    const std::shared_ptr<nusim::srv::Teleport::Request> request,
+    std::shared_ptr<nusim::srv::Teleport::Response>)
+    {
+      x_ = request->x;
+      y_ = request->y;
+      theta_ = request->theta;
+    }
+
 
 
   // Declare member variables
@@ -88,11 +119,18 @@ private:
   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_publisher_;
 
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_service;
+  rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_service;
 
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
   int timestep_;
   int rate_;
+  double x0_;
+  double y0_;
+  double theta0_;
+  double x_;
+  double y_;
+  double theta_;
 
 
 };
