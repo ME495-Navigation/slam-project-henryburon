9993b69ef7cc5bd915c50c0102a5df0987ef1ba7
Henry Buron <henryburon34@gmail.com>
2024-01-22 18:19:35 -0600

    Got a marker array to show up (just a cube) but looks like everything to this point is working

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -11,6 +11,7 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "nusim/srv/teleport.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 
 using namespace std::chrono_literals;
 
@@ -24,15 +25,23 @@ public:
     declare_parameter("x0", 0.0);
     declare_parameter("y0", 0.0);
     declare_parameter("theta0", 0.0);
+    declare_parameter("walls.arena_x_length", 5.0);
+    declare_parameter("walls.arena_y_length", 5.0);
 
 
     rate_ = get_parameter("rate").get_parameter_value().get<int>();
     x0_ = get_parameter("x0").get_parameter_value().get<double>();
     y0_ = get_parameter("y0").get_parameter_value().get<double>();
     theta0_ = get_parameter("theta0").get_parameter_value().get<double>();
+    arena_x_length_ = get_parameter("walls.arena_x_length").get_parameter_value().get<double>();
+    arena_y_length_ = get_parameter("walls.arena_y_length").get_parameter_value().get<double>();
+
 
     // Create a publisher for UInt64 messages on the "~/timestep" topic.
     timestep_publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
+
+    // Wall publisher
+    walls_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("~/walls", 10);
 
     // Create a timer that triggers the timer_callback function with a frequency of rate Hz
     timer_ = this->create_wall_timer(std::chrono::milliseconds(1000 / rate_), std::bind(&Nusim::timer_callback, this));
@@ -52,9 +61,14 @@ public:
     x_ = x0_;
     y_ = y0_;
     theta_ = theta0_;
+
+    create_walls_();
   }
 
 private:
+
+
+
 
   void timer_callback()
   {
@@ -64,13 +78,8 @@ private:
     // Publish the UInt64 message
     timestep_publisher_->publish(message);
 
-    // Also print the message to the command line
-    // std::string message_str = std::to_string(message.data);
-    // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message_str.c_str());
-
-    geometry_msgs::msg::TransformStamped t;
-
     // Read message content and assign it to corresponding tf variables
+    geometry_msgs::msg::TransformStamped t;
     t.header.stamp = this->get_clock()->now();
     t.header.frame_id = "nusim/world";
     t.child_frame_id = "red/base_footprint";
@@ -90,6 +99,8 @@ private:
 
     // Send the transform
     tf_broadcaster_->sendTransform(t);
+
+    walls_publisher_->publish(wall_markers_array_);
 
   }
 
@@ -112,16 +123,52 @@ private:
       theta_ = request->theta;
     }
 
+  void create_walls_()
+  {
+    visualization_msgs::msg::Marker wall_mark_;
+    wall_mark_.header.frame_id = "nusim/world";
+    wall_mark_.header.stamp = get_clock()->now();
+    wall_mark_.id = 0;
+    wall_mark_.type = visualization_msgs::msg::Marker::CUBE;
+    wall_mark_.action = visualization_msgs::msg::Marker::ADD;
+
+    wall_mark_.pose.position.x = 3.0;
+    wall_mark_.pose.position.y = 0.0;
+    wall_mark_.pose.position.z = 0.0;
+
+    wall_mark_.pose.orientation.x = 0.0;
+    wall_mark_.pose.orientation.y = 0.0;
+    wall_mark_.pose.orientation.z = 0.0;
+    wall_mark_.pose.orientation.w = 0.0;
+
+    wall_mark_.scale.x = 1.0;
+    wall_mark_.scale.y = 1.0;
+    wall_mark_.scale.z = 1.0;
+
+    wall_mark_.color.r = 0.0f;
+    wall_mark_.color.g = 1.0f;
+    wall_mark_.color.b = 0.0f;
+    wall_mark_.color.a = 1.0f;
+
+    wall_markers_array_.markers.push_back(wall_mark_);
+
+
+
+  }
+
 
 
   // Declare member variables
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_publisher_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr walls_publisher_;
 
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_service;
   rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_service;
 
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  visualization_msgs::msg::MarkerArray wall_markers_array_;
 
   int timestep_;
   int rate_;
@@ -131,6 +178,8 @@ private:
   double x_;
   double y_;
   double theta_;
+  double arena_x_length_;
+  double arena_y_length_;
 
 
 };
