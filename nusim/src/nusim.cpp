92e5b5a52bc65f2608fed01431492cb24bbea186
Henry Buron <henryburon34@gmail.com>
2024-01-22 20:20:01 -0600

    Some of the obstacles are showing up

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -25,8 +25,11 @@ public:
     declare_parameter("x0", 0.0);
     declare_parameter("y0", 0.0);
     declare_parameter("theta0", 0.0);
-    declare_parameter("walls.arena_x_length", 5.0);
-    declare_parameter("walls.arena_y_length", 5.0);
+    declare_parameter("walls.arena_x_length", 10.0);
+    declare_parameter("walls.arena_y_length", 10.0);
+    declare_parameter("obstacles.x", std::vector<double> {});
+    declare_parameter("obstacles.y", std::vector<double> {});
+    declare_parameter("obstacles.r", 0.038);
 
     wall_thickness_ = 0.1;
 
@@ -37,13 +40,17 @@ public:
     theta0_ = get_parameter("theta0").get_parameter_value().get<double>();
     arena_x_length_ = get_parameter("walls.arena_x_length").get_parameter_value().get<double>();
     arena_y_length_ = get_parameter("walls.arena_y_length").get_parameter_value().get<double>();
+    obstacles_x_ = get_parameter("obstacles.x").get_parameter_value().get<std::vector<double>>();
+    obstacles_y_ = get_parameter("obstacles.y").get_parameter_value().get<std::vector<double>>();
+    obstacles_r_ = get_parameter("obstacles.r").get_parameter_value().get<double>();
 
 
-    // Create a publisher for UInt64 messages on the "~/timestep" topic.
+    // Publishers
     timestep_publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
-
-    // Wall publisher
     walls_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("~/walls", 10);
+    obstacles_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("~/obstacles", 10);
+
+
 
     // Create a timer that triggers the timer_callback function with a frequency of rate Hz
     timer_ = this->create_wall_timer(std::chrono::milliseconds(1000 / rate_), std::bind(&Nusim::timer_callback, this));
@@ -65,6 +72,7 @@ public:
     theta_ = theta0_;
 
     create_walls_();
+    create_obstacles();
   }
 
 private:
@@ -103,6 +111,7 @@ private:
     tf_broadcaster_->sendTransform(t);
 
     walls_publisher_->publish(wall_markers_array_);
+    obstacles_publisher_->publish(obstacles_markers_array_);
 
   }
 
@@ -210,12 +219,43 @@ private:
     wall_markers_array_.markers.push_back(wall_mark_4);
   }
 
+  void create_obstacles()
+  {
+    if (obstacles_x_.size() != obstacles_y_.size())
+    {
+      RCLCPP_ERROR(this->get_logger(), "Error: obstacles/x and obstacles/y should be the same length.");
+      rclcpp::shutdown();
+    }
+    const auto marker_array_size = obstacles_x_.size();
+
+    for (long unsigned int i = 0; i < marker_array_size; i++) {
+      visualization_msgs::msg::Marker marker;
+      marker.header.frame_id = "nusim/world";
+      marker.header.stamp = get_clock()->now();
+      marker.id = i;
+      marker.type = visualization_msgs::msg::Marker::CYLINDER;
+      marker.action = visualization_msgs::msg::Marker::ADD;
+      marker.pose.position.x = obstacles_x_.at(i);
+      marker.pose.position.y = obstacles_y_.at(i);
+      marker.pose.position.z = 0.125;
+      marker.scale.x = 2.0 * obstacles_r_;
+      marker.scale.y = 2.0 * obstacles_r_;
+      marker.scale.z = 0.25;
+      marker.color.r = 1.0;
+      marker.color.g = 0.0;
+      marker.color.b = 0.0;
+      marker.color.a = 1.0;
+      obstacles_markers_array_.markers.push_back(marker);
+  }
+  }
+
 
 
   // Declare member variables
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_publisher_;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr walls_publisher_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr obstacles_publisher_;
 
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_service;
   rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_service;
@@ -223,7 +263,9 @@ private:
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
   visualization_msgs::msg::MarkerArray wall_markers_array_;
+  visualization_msgs::msg::MarkerArray obstacles_markers_array_;
 
+  // size_t timestep_;
   int timestep_;
   int rate_;
   double x0_;
@@ -235,6 +277,9 @@ private:
   double arena_x_length_;
   double arena_y_length_;
   double wall_thickness_;
+  std::vector<double> obstacles_x_;
+  std::vector<double> obstacles_y_;
+  double obstacles_r_;
 
 
 };
