f82f117c3ad186b2b3de91525a1ce34263475a19
Henry Buron <henryburon34@gmail.com>
2024-02-06 00:55:07 -0600

    Basically finished E.1 Didn't test the sensor_data_callback yet

--- nuturtle_control/src/turtle_control.cpp
+++ nuturtle_control/src/turtle_control.cpp
@@ -23,24 +23,24 @@ public:
     declare_parameter("track_width", -5.0);
     declare_parameter("motor_cmd_max", -5.0);
     declare_parameter("motor_cmd_per_rad_sec", -5.0);
-
+    declare_parameter("encoder_ticks_per_rad", -5.0);
   
     wheel_radius_ = get_parameter("wheel_radius").get_parameter_value().get<double>();
     track_width_ = get_parameter("track_width").get_parameter_value().get<double>();
     motor_cmd_max_ = get_parameter("motor_cmd_max").get_parameter_value().get<double>();
     motor_cmd_per_rad_sec_ = get_parameter("motor_cmd_per_rad_sec").get_parameter_value().get<double>();
-
+    encoder_ticks_per_rad_ = get_parameter("encoder_ticks_per_rad").get_parameter_value().get<double>();
 
 
     // Publishers
     wheel_cmd_pub = create_publisher<nuturtlebot_msgs::msg::WheelCommands>("wheel_cmd", 10);
-    // joint_states_pub = create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+    joint_states_pub = create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
 
     // Subscribers
     cmd_vel_sub = create_subscription<geometry_msgs::msg::Twist>(
       "cmd_vel", 10, std::bind(&TurtleControl::cmd_vel_callback, this, std::placeholders::_1));
-    // sensor_data_sub = create_subscription<nuturtlebot_msgs::msg::SensorData>(
-    //   "sensor_data", 10, std::bind(&TurtleControl::sensor_data_callback, this, std::placeholders::_1));
+    sensor_data_sub = create_subscription<nuturtlebot_msgs::msg::SensorData>(
+      "sensor_data", 10, std::bind(&TurtleControl::sensor_data_callback, this, std::placeholders::_1));
 
     // Setup Functions
     check_params();
@@ -74,21 +74,83 @@ private:
     wheel_cmd_.left_velocity = required_wheels.phi_l;
     wheel_cmd_.right_velocity = required_wheels.phi_r;
 
+    // Ensure motor (wheel) commands are within specified interval
+    if (wheel_cmd_.left_velocity > motor_cmd_max_)
+    {
+      wheel_cmd_.left_velocity = motor_cmd_max_;
+    }
+    else if (wheel_cmd_.left_velocity < -motor_cmd_max_)
+    {
+      wheel_cmd_.left_velocity = -motor_cmd_max_;
+    }
+    if (wheel_cmd_.right_velocity > motor_cmd_max_)
+    {
+      wheel_cmd_.right_velocity = motor_cmd_max_;
+    }
+    else if (wheel_cmd_.right_velocity < -motor_cmd_max_)
+    {
+      wheel_cmd_.right_velocity = -motor_cmd_max_;
+    }
+
     // Publish the wheel_cmd message
     wheel_cmd_pub->publish(wheel_cmd_);
 
   }
 
-  // void sensor_data_callback(const nuturtlebot_msgs::msg::SensorData & msg) const
-  // {
+  void sensor_data_callback(const nuturtlebot_msgs::msg::SensorData & msg)
+  {
+    // Create a JointState message
+    sensor_msgs::msg::JointState js;
 
-  // }
+    // Create the message (header, name, position, velocity, effort)
+    js.header.stamp = msg.stamp;
+    js.name = {"wheel_left_joint", "wheel_right_joint"};
+
+    // Load position and velocity
+    if (flag_stamp < 0.0) // If it's the first reading...
+    {
+      js.position = {0.0, 0.0};
+      js.velocity = {0.0, 0.0};
+    }
+    else
+    {
+      // Time since last reading
+      double elapsed_time = msg.stamp.sec + msg.stamp.nanosec * 1e-9 - flag_stamp; // Convert units to seconds
+
+      js.position = 
+        {msg.left_encoder / encoder_ticks_per_rad_,
+         msg.right_encoder / encoder_ticks_per_rad_};
+
+      if (elapsed_time > 0.0)
+      {
+        js.velocity = {js.position[0] / elapsed_time,
+                       js.position[1] / elapsed_time};
+      }
+
+      // Handle division by zero scenario
+      else
+      {
+        js.velocity = {0.0, 0.0};
+      }
+    }
+
+    // Set stamp and publish joint states
+    flag_stamp = msg.stamp.sec + msg.stamp.nanosec * 1e-9; // Can also use: get_clock()->now()
+    joint_states_pub->publish(js);
+
+
+    
+  }
 
 
 
   void check_params()
   {
-    if (wheel_radius_ < 0 || track_width_ < 0)
+    if (wheel_radius_ < 0 
+        || track_width_ < 0
+        || motor_cmd_max_ < 0
+        || motor_cmd_per_rad_sec_ < 0
+        || encoder_ticks_per_rad_ < 0)
     {
       RCLCPP_ERROR_STREAM(this->get_logger(), "Not all required parameters are defined in diff_params.yaml.");
       rclcpp::shutdown();
@@ -98,15 +160,16 @@ private:
 
 
 
+
 /// Declare member variables ///
 
 // Publishers
 rclcpp::Publisher<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr wheel_cmd_pub;
-// rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_states_pub;
+rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_states_pub;
 
 // Subscribers
 rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
-// rclcpp::Subscription<nuturtlebot_msgs::msg::SensorData>::SharedPtr sensor_data_sub;
+rclcpp::Subscription<nuturtlebot_msgs::msg::SensorData>::SharedPtr sensor_data_sub;
 
 // Variables
 int rate_;
@@ -114,6 +177,8 @@ double wheel_radius_;
 double track_width_;
 double motor_cmd_max_;
 double motor_cmd_per_rad_sec_;
+double encoder_ticks_per_rad_;
+double flag_stamp = -1.0;
 
 };
 
