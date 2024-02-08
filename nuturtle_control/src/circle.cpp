b44c603e9833cd40ecf8423bf5d4c60874998ce2
Henry Buron <henryburon34@gmail.com>
2024-02-07 18:46:26 -0600

    Stop, reverse, and control services seem to be working

--- nuturtle_control/src/circle.cpp
+++ nuturtle_control/src/circle.cpp
@@ -28,6 +28,22 @@ public:
             this,
             std::placeholders::_1,
             std::placeholders::_2));
+
+      reverse_init = create_service<std_srvs::srv::Empty>(
+         "/reverse",
+         std::bind(
+            &Circle::reverse_callback,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2));
+
+      stop_init = create_service<std_srvs::srv::Empty>(
+         "/stop",
+         std::bind(
+            &Circle::stop_callback,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2));
       
       // Timer
       timer_ = create_wall_timer(std::chrono::milliseconds(1000 / freq_),
@@ -36,17 +52,46 @@ public:
 private:
    void timer_callback()
    {
-      // Publish the cmd_vel
-      cmd_vel_pub->publish(arc_);
+      if (do_arc_ == true)
+      {
+         // If not stopped, publish the arc's twist
+         cmd_vel_pub->publish(arc_);
+      }
+      
    }
 
    void control_callback(
       std::shared_ptr<nuturtle_control::srv::Control::Request> request,
       std::shared_ptr<nuturtle_control::srv::Control::Response>)
       {
-         // This changes the values of the cmd_vel that is published
+         // Changes the arc values to new values specified in service call
          arc_.linear.x = request->radius * request->velocity;
          arc_.angular.z = request->velocity;
+
+         do_arc_ = true;
+      }
+
+   void reverse_callback(
+      std::shared_ptr<std_srvs::srv::Empty::Request>,
+      std::shared_ptr<std_srvs::srv::Empty::Response>)
+      {
+         // Simply reverses the current direction
+         arc_.linear.x = -arc_.linear.x;
+         arc_.angular.z = -arc_.angular.z;
+
+         do_arc_ = true;
+      }
+
+   void stop_callback(
+      std::shared_ptr<std_srvs::srv::Empty::Request>,
+      std::shared_ptr<std_srvs::srv::Empty::Response>)
+      {
+         // Stops all motion
+         arc_.linear.x = 0.0;
+         arc_.angular.z = 0.0;
+         cmd_vel_pub->publish(arc_);
+
+         do_arc_ = false;
       }
    
 
@@ -58,12 +103,15 @@ rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
 
 // Services
 rclcpp::Service<nuturtle_control::srv::Control>::SharedPtr control_init;
+rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reverse_init;
+rclcpp::Service<std_srvs::srv::Empty>::SharedPtr stop_init;
 
 // Timer
 rclcpp::TimerBase::SharedPtr timer_;
 
 // Variables
 geometry_msgs::msg::Twist arc_;
+bool do_arc_ = true;
 
 };
 
