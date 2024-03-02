59e170aa2fbb9784fc787ebfae25150ffc7b69fb
Henry Buron <henryburon34@gmail.com>
2024-03-02 08:25:09 -0500

    fixed param file issue and added green obstacles just to test the map

--- nuslam/src/slam.cpp
+++ nuslam/src/slam.cpp
@@ -11,7 +11,8 @@
 ///    \param initial_pose (double): The initial pose of the robot.
 ///
 /// PUBLISHES:
-///    \param odom (nav_msgs::msg::Odometry): Publishes the computed odometry of the robot.
+///    \param green/odom (nav_msgs::msg::Odometry): Publishes the computed odometry of the robot.
+///    \param green/path (nav_msgs::msg::Path): Publishes the path of the green robot.
 ///
 /// SUBSCRIBES:
 ///    \param red/joint_states (sensor_msgs::msg::JointState): Subscribes to the joint state messages to compute odometry.
@@ -31,6 +32,8 @@
 #include "nuturtle_control/srv/initial_pose.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 
 /// \brief Computes and publishes odometry from the joint states.
 class Slam : public rclcpp::Node
@@ -46,6 +49,9 @@ public:
     declare_parameter("wheel_right", "green/wheel_right_link");
     declare_parameter("wheel_radius", 0.033);
     declare_parameter("track_width", 0.160);
+    declare_parameter("obstacles.x", std::vector<double>{});
+    declare_parameter("obstacles.y", std::vector<double>{});
+    declare_parameter("obstacles.r", 0.038);
 
     body_id_ = get_parameter("body_id").get_value<std::string>();
     odom_id_ = get_parameter("odom_id").get_value<std::string>();
@@ -53,15 +59,22 @@ public:
     wheel_right_ = get_parameter("wheel_right").get_value<std::string>();
     wheel_radius_ = get_parameter("wheel_radius").get_value<double>();
     track_width_ = get_parameter("track_width").get_value<double>();
+    obstacles_x_ = get_parameter("obstacles.x").get_parameter_value().get<std::vector<double>>();
+    obstacles_y_ = get_parameter("obstacles.y").get_parameter_value().get<std::vector<double>>();
+    obstacles_r_ = get_parameter("obstacles.r").get_parameter_value().get<double>();
 
     // Publishers
     green_odom_pub = create_publisher<nav_msgs::msg::Odometry>("green/odom", 10);
     green_path_pub = create_publisher<nav_msgs::msg::Path>("green/path", 10);
+    green_obstacles_pub = create_publisher<visualization_msgs::msg::MarkerArray>("green/obstacles", 10);
 
     // Subscribers
     joint_states_sub = create_subscription<sensor_msgs::msg::JointState>(
       "blue/joint_states", 10,
       std::bind(&Slam::joint_states_callback, this, std::placeholders::_1));
+    fake_sensor_sub = create_subscription<visualization_msgs::msg::MarkerArray>(
+      "/fake_sensor", 10,
+      std::bind(&Slam::fake_sensor_callback, this, std::placeholders::_1));
 
     // Broadcasters
     odom_body_broadcaster =
@@ -84,6 +97,60 @@ public:
   }
 
 private:
+
+  void fake_sensor_callback(const visualization_msgs::msg::MarkerArray::SharedPtr msg)
+  {
+    create_obstacles();
+
+
+
+
+
+
+
+
+
+  }
+
+  /// \brief Creates static cylindrical obstacles in the simulation
+  void create_obstacles()
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard this!");
+
+    // log obstacles x
+    for (long unsigned int i = 0; i < obstacles_x_.size(); i++) {
+      RCLCPP_INFO(this->get_logger(), "Obstacle x: %f", obstacles_x_.at(i));
+    }
+
+    if (obstacles_x_.size() != obstacles_y_.size()) {
+      RCLCPP_ERROR(
+        this->get_logger(), "Error: obstacles/x and obstacles/y should be the same length.");
+      rclcpp::shutdown();
+    }
+    const auto num_markers = obstacles_x_.size();
+
+    for (long unsigned int i = 0; i < num_markers; i++) {
+      visualization_msgs::msg::Marker obs;
+      obs.header.frame_id = "map";
+      obs.header.stamp = get_clock()->now();
+      obs.id = i;
+      obs.type = visualization_msgs::msg::Marker::CYLINDER;
+      obs.action = visualization_msgs::msg::Marker::ADD;
+
+      obs.pose.position.x = obstacles_x_.at(i);
+      obs.pose.position.y = obstacles_y_.at(i);
+      obs.pose.position.z = 0.125;
+
+      obs.scale.x = 2.0 * obstacles_r_;
+      obs.scale.y = 2.0 * obstacles_r_;
+      obs.scale.z = 0.25;
+
+      obs.color.g = 1.0;
+      obs.color.a = 1.0;
+      obstacles_markers_array_.markers.push_back(obs);
+      green_obstacles_pub->publish(obstacles_markers_array_);
+    }
+  }
 
   /// \brief Callback function for the initial_pose service.
   void initial_pose_callback(
@@ -200,9 +267,11 @@ private:
 // Publishers
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr green_odom_pub;
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr green_path_pub;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr green_obstacles_pub;
 
 // Subscribers
   rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_states_sub;
+  rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr fake_sensor_sub;
 
 // Broadcasters
   std::unique_ptr<tf2_ros::TransformBroadcaster> odom_body_broadcaster;
@@ -223,6 +292,12 @@ private:
   double wheel_radius_;
   double track_width_;
   nav_msgs::msg::Path path_msg_;
+  std::vector<double> obstacles_x_;
+  std::vector<double> obstacles_y_;
+  double obstacles_r_;
+  visualization_msgs::msg::MarkerArray obstacles_markers_array_;
+
+
 
 };
 
